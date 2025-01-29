f66d24c507e26cc541cc43ac0bf7a68da06c5fc0
Sergei Kasianenko <41939763+urchinpro@users.noreply.github.com>
2025-01-29 12:16:11 +0800

    Update api/researches/views.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- api/researches/views.py
+++ api/researches/views.py
@@ -32,7 +32,8 @@ from directory.models import (
     ReleationsFT,
     ParaclinicTemplateNameDepartment,
     ParaclinicFieldTemplateDepartment,
-    ConstructorEditAccessResearch, Researches,
+    ConstructorEditAccessResearch,
+    Researches,
 )
 from directory.sql_func import get_constructor_edit_access_by_research_id, get_constructor_edit_access_by_department_or_doctor
 from directory.utils import get_researches_details
